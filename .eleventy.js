8b3e576bdc9230c354a62c68bb0eb707edd54f63
Mohammed Khan <mkha5549@stu.raritanval.edu>
2023-12-08 20:29:31 -0500

    fixing the background-image error

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 // Copy `img/` to `_site/img`
-  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/**/*.js");
   eleventyConfig.addWatchTarget("./src/**/*.html");
   eleventyConfig.addWatchTarget("./src/**/*.js");
